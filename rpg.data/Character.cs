25b35fe4304becce081b2260b969ef7635b7367f
Rowida <rowidanagah462000@gmail.com>
2022-08-23 21:21:42 +0200

    Adding first part of MVC -> model

--- rpg.data/Character.cs
+++ rpg.data/Character.cs
@@ -4,9 +4,15 @@ namespace rpg.data
 {
     public class Character
     {
+        // no default vals would be assigned...
         public int id { get; set; } 
-
         public string name { get; set; }
+        public string HitPoints { get; set; }
+        public int Strength { get; set; } 
+        public int Defense { get; set; }
+        public int Intelligence { get; set; }
+        // we will work with enum val of classes 
+        public RpgClass rgpclass { get; set; }
 
 
 
---
+++ rpg.data/RpgClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rpg.data
+{
+
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
